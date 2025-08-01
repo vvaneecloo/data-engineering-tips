475415b8fcaeb65ac0aecab6eb0f3e25e4dacf73
VV <66221043+vvaneecloo@users.noreply.github.com>
2025-08-01 15:56:24 +0200

    (fix) indentation

--- dbt/override_source.sql
+++ dbt/override_source.sql
@@ -1,15 +1,15 @@
 /*
-        This macro aims to override the source dbt macro to automatically add a limit to all
-        relations, e.g:
+    This macro aims to override the source dbt macro to automatically add a limit to all
+    relations, e.g:
 
-        `{{ source('prod', 'some_table') }}` becomes `select * from prod.some_table limit 1000`
-        instead of `select * from prod.some_table`
+    `{{ source('prod', 'some_table') }}` becomes `select * from prod.some_table limit 1000`
+    instead of `select * from prod.some_table`
 
-        To use this in your dbt project you'd have to modify:
-            - target.name / profile.name depending on your use case,
-            - the object you're comparing to (here 'dev')
-            - the limit you want to add (here '1000'),
-            - you can also change the limit logic (limit / tablesample) based on your data warehouse
+    To use this in your dbt project you'd have to modify:
+        - target.name / profile.name depending on your use case,
+        - the object you're comparing to (here 'dev')
+        - the limit you want to add (here '1000'),
+        - you can also change the limit logic (limit / tablesample) based on your data warehouse
 */
 
 {% macro source(model_name) %}
