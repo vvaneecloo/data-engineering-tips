7ae7f5628a719de321b54114919330d95e501d6b
VV <66221043+vvaneecloo@users.noreply.github.com>
2025-08-04 11:22:14 +0200

    (fix) documentation + code

--- dbt/insert_row_count.sql
+++ dbt/insert_row_count.sql
@@ -1,25 +1,27 @@
+/*
+    These macros aim to insert the row count of a table after it has been updated.
+    This is especially useful to use as a statistics & alerting table.
+
+    To use this in your dbt project you'd have to:
+
+        1) Add this piece of code to `dbt_project.yml`:
+            ```
+            on-run-start: 
+                - "{{ create_monitoring_table_if_not_exists() }}"
+            ```
+
+        2) Add this other piece of code in your `dbt_project.yml` (depending on what models you want to monitor):
+            ```
+            +post-hook:
+                - "{{ insert_row_count() }}"
+            ```
+*/
+
 {% macro insert_row_count() %}
     {%- if target.name == 'prod' -%}
         {%- set accepted_commands = ['run', 'build'] -%}
         {%- if invocation_args_dict.get('which') in accepted_commands -%}
-            {{ _create_monitoring_table_if_not_exists() }}
-            {{ _append_row_count_to_row_count_monitoring() }}
-        {%- endif -%}
-    {%- endif -%}
-{% endmacro %}
-
-{% macro _create_monitoring_table_if_not_exists() %}
-        create table if not exists {{ _get_monitoring_target_schema() }}.row_count_monitoring (
-            execution_time timestamp,
-            model string,
-            invocation_id string,
-            package_name string,
-            row_count bigint
-        );
-{% endmacro %}
-
-{% macro _append_row_count_to_row_count_monitoring() %}
-        insert into {{ _get_monitoring_target_schema() }}.row_count_monitoring
+            insert into {{ _get_monitoring_target_schema() }}.row_count_monitoring
                 select 
                     current_timestamp() as execution_time,
                     '{{ this }}' as model,
@@ -28,6 +30,20 @@
                     count(1) as row_count
                 from 
                     {{ this }};
+        {%- endif -%}
+    {%- endif -%}
+{% endmacro %}
+
+{% macro create_monitoring_table_if_not_exists() %}
+    {%- if target.name == 'prod' -%}
+        create table if not exists {{ _get_monitoring_target_schema() }}.row_count_monitoring (
+            execution_time timestamp,
+            model string,
+            invocation_id string,
+            package_name string,
+            row_count bigint
+        );
+    {%- endif -%}
 {% endmacro %}
 
 {% macro _get_monitoring_target_schema() %}
@@ -41,3 +57,4 @@
        {{ return("dev_" ~ db_name) }}
     {%- endif -%}
 {% endmacro %}
+
