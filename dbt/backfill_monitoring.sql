a962d286f3eea5801636401b822805272a11522d
VV <66221043+vvaneecloo@users.noreply.github.com>
2025-08-26 11:18:45 +0200

    (fix) cloud agnostic > databricks + dry code

--- dbt/backfill_monitoring.sql
+++ dbt/backfill_monitoring.sql
@@ -1,28 +1,27 @@
 {% macro log_incremental() %}
+    {% set common_vars %}
+        current_timestamp() as timestamp
+        , "{{ invocation_id | string }}" as invocation_id
+        , "{{ run_started_at | string }}" as run_started_at
+        , "{{ is_incremental() | string }}" :: boolean  as is_full_refresh
+        , "{{ env_var('RUN_ID', 'None') | string }}" :: string as run_id
+        , "{{ env_var('JOB_ID', 'None') | string }}" :: string as job_id
+    {% endset %}
+
     {% if is_incremental() %}
         array_append(
             _metadata,
             struct(
-              current_timestamp() as timestamp
-              , '{{ invocation_id | string }}' as invocation_id
-              , '{{ run_started_at | string }}' as run_started_at
-              , '{{ env_var("DATA_INTERVAL_START", "None") ~ " - " ~ env_var("DATA_INTERVAL_END", "None") }}' :: string as data_interval
-              , '{{ is_incremental()  }}' :: boolean  as is_full_refresh
-              , '{{ env_var("DATABRICKS_RUN_ID", "None") }}' :: string as databricks_run_id
-              , '{{ env_var("DATABRICKS_JOB_ID", "None") }}' :: string as databricks_job_id
+              {{ common_vars }}
+              , "{{ env_var('DATA_INTERVAL_START', 'None') ~ ' - ' ~ env_var('DATA_INTERVAL_END', 'None') | string }}" :: string as data_interval
             )
         ) as _metadata
     {% else %}
         array(
             struct(
-              current_timestamp() as timestamp
-              , '{{ invocation_id | string }}' as invocation_id
-              , '{{ run_started_at | string }}' as run_started_at
-              , '{{ env_var("BACKFILL_INTERVAL_START", "None") ~ " - " ~ env_var("BACKFILL_INTERVAL_END", "None") }}' :: string as data_interval
-              , '{{ is_incremental()  }}' :: boolean  as is_full_refresh
-              , '{{ env_var("DATABRICKS_RUN_ID", "None") }}' :: string as databricks_run_id
-              , '{{ env_var("DATABRICKS_JOB_ID", "None") }}' :: string as databricks_job_id
+              {{ common_vars }}
+              , "{{ env_var('MONDAY_WEEKS_TO_BACKFILL', 'None') | string }}" :: string as data_interval
             )
-        )
+        ) as _metadata
     {% endif %}
 {% endmacro %}
