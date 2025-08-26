55f07d129a07e96e203b7b0f7b14f1c843c82f63
VV <66221043+vvaneecloo@users.noreply.github.com>
2025-08-26 09:43:45 +0200

    Create backfill_monitoring.sql

---
+++ dbt/backfill_monitoring.sql
@@ -0,0 +1,11 @@
+{% macro backfill_monitoring() %}
+  , array(
+        struct(
+          current_timestamp() as updated_at,
+          '{{ invocation_id }}' as run_id,
+          '{{ var("run_type", None) }}' as run_type,
+          '{{ var("data_interval_start", "NA") }}' as backfill_start,
+          '{{ var("data_interval_end", "NA") }}' as backfill_end
+        )
+      )
+{% endmacro %}
