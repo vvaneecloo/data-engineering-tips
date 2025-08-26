e13553ffce8ed1969530b21b22302b8aeeec4e89
VV <66221043+vvaneecloo@users.noreply.github.com>
2025-08-26 10:53:15 +0200

    (fix) simplify macro

--- dbt/backfill_monitoring.sql
+++ dbt/backfill_monitoring.sql
@@ -1,44 +1,28 @@
 {% macro log_incremental() %}
-  {% if is_incremental() %}
-        , array_append(
+    {% if is_incremental() %}
+        array_append(
             _metadata,
             struct(
               current_timestamp() as timestamp
-              , {{ invocation_id }} as invocation_id
-              , {{ run_started_at }} as run_started_at
-              , case 
-                when {{ is_incremental() }} 
-                    then concat(
-                      '{{ env_var("DATA_INTERVAL_START", "None in env") }}', 
-                      ' - ', 
-                      '{{ env_var("DATA_INTERVAL_END", "None in env") }}'
-                    )
-                    else '{{ env_var("MONDAY_WEEKS_TO_BACKFILL", "None in env") }}'
-                end as data_interval
-              , '{{ env_var("FULL_REFRESH", "None in env") }}' as is_full_refresh
-              , '{{ env_var("DATABRICKS_RUN_ID", "None in env") }}' as databricks_run_id
-              , '{{ env_var("DATABRICKS_JOB_ID", "None in env") }}' as databricks_job_id
+              , '{{ invocation_id | string }}' as invocation_id
+              , '{{ run_started_at | string }}' as run_started_at
+              , '{{ env_var("DATA_INTERVAL_START", "None") ~ " - " ~ env_var("DATA_INTERVAL_END", "None") }}' :: string as data_interval
+              , '{{ is_incremental()  }}' :: boolean  as is_full_refresh
+              , '{{ env_var("DATABRICKS_RUN_ID", "None") }}' :: string as databricks_run_id
+              , '{{ env_var("DATABRICKS_JOB_ID", "None") }}' :: string as databricks_job_id
             )
         ) as _metadata
     {% else %}
-        , array(
+        array(
             struct(
               current_timestamp() as timestamp
-              , {{ invocation_id }} as invocation_id
-              , {{ run_started_at }} as run_started_at
-              , case 
-                when {{ is_incremental() }} 
-                    then concat(
-                      '{{ env_var("DATA_INTERVAL_START", "None in env") }}', 
-                      ' - ', 
-                      '{{ env_var("DATA_INTERVAL_END", "None in env") }}'
-                    )
-                    else '{{ env_var("MONDAY_WEEKS_TO_BACKFILL", "None in env") }}'
-                end as data_interval
-              , '{{ env_var("FULL_REFRESH", "None in env") }}' as is_full_refresh
-              , '{{ env_var("DATABRICKS_RUN_ID", "None in env") }}' as databricks_run_id
-              , '{{ env_var("DATABRICKS_JOB_ID", "None in env") }}' as databricks_job_id
+              , '{{ invocation_id | string }}' as invocation_id
+              , '{{ run_started_at | string }}' as run_started_at
+              , '{{ env_var("BACKFILL_INTERVAL_START", "None") ~ " - " ~ env_var("BACKFILL_INTERVAL_END", "None") }}' :: string as data_interval
+              , '{{ is_incremental()  }}' :: boolean  as is_full_refresh
+              , '{{ env_var("DATABRICKS_RUN_ID", "None") }}' :: string as databricks_run_id
+              , '{{ env_var("DATABRICKS_JOB_ID", "None") }}' :: string as databricks_job_id
             )
         )
     {% endif %}
-  {% endmacro %}
+{% endmacro %}
