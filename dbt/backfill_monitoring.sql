e9859df2d2ec473ec5ac664f9ebf7aecac9f188d
VV <66221043+vvaneecloo@users.noreply.github.com>
2025-08-26 10:25:09 +0200

    (fix) add new columns

--- dbt/backfill_monitoring.sql
+++ dbt/backfill_monitoring.sql
@@ -1,18 +1,20 @@
 {% macro backfill_monitoring() %}
   {% if is_incremental() %}
         , array_append(
-            _updated_at, 
+            _metadata,
             struct(
               current_timestamp() as timestamp
-              {% if is_incremental() %}
-                , concat(
-                  '{{ env_var("DATA_INTERVAL_START", "None in env") }}', 
-                  ' - ', 
-                  '{{ env_var("DATA_INTERVAL_END", "None in env") }}'
-                ) as data_interval
-              {% else %}
-                '{{ env_var("MONDAY_WEEKS_TO_BACKFILL", "None in env") }}' as data_interval
-              {% endif %}
+              , {{ invocation_id }} as invocation_id
+              , {{ run_started_at }} as run_started_at
+              , case 
+                when {{ is_incremental() }} 
+                    then concat(
+                      '{{ env_var("DATA_INTERVAL_START", "None in env") }}', 
+                      ' - ', 
+                      '{{ env_var("DATA_INTERVAL_END", "None in env") }}'
+                    )
+                    else '{{ env_var("MONDAY_WEEKS_TO_BACKFILL", "None in env") }}'
+                end as data_interval
               , '{{ env_var("FULL_REFRESH", "None in env") }}' as is_full_refresh
               , '{{ env_var("DATABRICKS_RUN_ID", "None in env") }}' as databricks_run_id
               , '{{ env_var("DATABRICKS_JOB_ID", "None in env") }}' as databricks_job_id
@@ -22,19 +24,21 @@
         , array(
             struct(
               current_timestamp() as timestamp
-              {% if is_incremental() %}
-                , concat(
-                  '{{ env_var("DATA_INTERVAL_START", "None in env") }}', 
-                  ' - ', 
-                  '{{ env_var("DATA_INTERVAL_END", "None in env") }}'
-                ) as data_interval
-              {% else %}
-                '{{ env_var("MONDAY_WEEKS_TO_BACKFILL", "None in env") }}' as data_interval
-              {% endif %}
+              , {{ invocation_id }} as invocation_id
+              , {{ run_started_at }} as run_started_at
+              , case 
+                when {{ is_incremental() }} 
+                    then concat(
+                      '{{ env_var("DATA_INTERVAL_START", "None in env") }}', 
+                      ' - ', 
+                      '{{ env_var("DATA_INTERVAL_END", "None in env") }}'
+                    )
+                    else '{{ env_var("MONDAY_WEEKS_TO_BACKFILL", "None in env") }}'
+                end as data_interval
               , '{{ env_var("FULL_REFRESH", "None in env") }}' as is_full_refresh
               , '{{ env_var("DATABRICKS_RUN_ID", "None in env") }}' as databricks_run_id
               , '{{ env_var("DATABRICKS_JOB_ID", "None in env") }}' as databricks_job_id
             )
-        ) as _metadata
+        )
     {% endif %}
   {% endmacro %}
